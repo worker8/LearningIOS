6035fe56c6ef68cd7b5eb0ac88741727640ce01c
Tan Jun Rong <junrong@mercari.com>
2023-12-27 09:14:58 +0900

    Initial Commit

---
+++ LearniOS2/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  LearniOS2
+//
+//  Created by Tan Jun Rong on 2023/12/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LearniOS2/LearniOS2App.swift
@@ -0,0 +1,17 @@
+//
+//  LearniOS2App.swift
+//  LearniOS2
+//
+//  Created by Tan Jun Rong on 2023/12/27.
+//
+
+import SwiftUI
+
+@main
+struct LearniOS2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
